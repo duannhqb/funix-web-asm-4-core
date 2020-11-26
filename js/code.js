59aa04ca914697d249b5a3e33d79e4c06ec29635
DuanNH <duannhqb@gmail.com>
2020-11-27 00:34:47 +0700

    update chatbot

--- js/code.js
+++ js/code.js
@@ -1,4 +1,3 @@
-<!--Start of Tawk.to Script-->
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 	var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -14,3 +13,10 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-CHXV63BRKW');
+
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MJ4PMN7');
